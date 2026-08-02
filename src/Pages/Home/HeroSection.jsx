8b015014d15b0d74f1dcec56609a4beaf7cd5a7e
az1213-dev <azapata12131@gmail.com>
2026-08-02 11:15:27 -0400

    Hero Section + Styles

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,11 +1,11 @@
 export default function HeroSection() {
 	return (
-		<section id="heroSection" className="hero-section">
-			<div className="hero--section-content-box">
-				<div className="hero--section-content">
+		<section id="heroSection" className="hero--section">
+			<div className="hero--section--content-box">
+				<div className="hero--section--content">
 					<p className="section--title">Hello, I'm Antonio</p>
-					<h1 className="hero--section-title">
-						<span className="hero--section-title-color">Full Stack</span>{" "}
+					<h1 className="hero--section--title">
+						<span className="hero--section--title--color">Full Stack</span>{" "}
 						<br />
 						Developer
 					</h1>
@@ -15,8 +15,8 @@ export default function HeroSection() {
 					</p>
 					<button className="btn btn-primary">Get In Touch</button>
 				</div>
-				<div className="hero--section-img">
-					<img src="https://placehold.co/600x400" alt="Hero Section" />
+				<div className="hero--section--img">
+					<img src="https://placehold.co/600x400" alt="Placeholder" />
 				</div>
 			</div>
 		</section>
